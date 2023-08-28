1f31d468fd4ff3f3837b48dcdb811d57db8b7cc3
Angel Baez <angelbaezsa@gmail.com>
2023-08-28 00:42:44 -0700

    mend

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,8 +1,31 @@
 //From now on you need to pack the whole markup of each modal in it's own file
 //(you shouldn't have only inputs in AddItemModal.js). Everything should be in AddItemModal.js
 //including handleSubmit and so on.
-import react from "react-dom";
-import { useContext } from "react";
-import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
-import "./WeatherCard.css";
-import { weatherOptions } from "../../utils/constants";
+import "./ModalWithForm.css";
+
+import React, { useState } from "react";
+
+function ModalWithForm({ children, onCloseModal, submitEvent, name }) {
+  return (
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal_content">
+        <button
+          className="form_close-button close-button"
+          onClick={onCloseModal}
+        ></button>
+
+        <h3 className="form_title">{name}</h3>
+
+        <form action="submit" onSubmit={submitEvent}>
+          {children}
+
+          <button className="form__button-submit" type="submit">
+            Add Garment
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
