4963693a64f002c0eb17d41d3a8e54d2bf6196c4
Angel Baez <angelbaezsa@gmail.com>
2023-08-28 00:23:45 -0700

    added code to modal with form

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,3 +1,26 @@
-//From now on you need to pack the whole markup of each modal in it's own file
-//(you shouldn't have only inputs in AddItemModal.js). Everything should be in AddItemModal.js
-//including handleSubmit and so on.
+// //From now on you need to pack the whole markup of each modal in it's own file
+// //(you shouldn't have only inputs in AddItemModal.js). Everything should be in AddItemModal.js
+// //including handleSubmit and so on.
+
+// import "./WeatherCard.css";
+// import { weatherOptions } from "../../utils/constants";
+
+// const WeatherCard = ({ day, type = "cloudy", temperature }) => {
+//   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+//   const weatherOption = weatherOptions.find((item) => {
+//     return item.type === type && item.day === day;
+//   });
+
+//   const imageSrc = weatherOption.url;
+//   return (
+//     <div className="weatherCard">
+//       <h3 className="weathercard__info">{`${temperature} ${currentTemperatureUnit.toUpperCase()}`}</h3>
+//       <img
+//         className="weathercard__image"
+//         src={imageSrc}
+//         alt={`Ilustration of weather card`}
+//       ></img>
+//     </div>
+//   );
+// };
+// export default WeatherCard;
