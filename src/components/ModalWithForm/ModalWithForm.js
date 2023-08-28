9f4ddc53569ff855bc532b1503d6c40bf89dba32
Angel Baez <angelbaezsa@gmail.com>
2023-08-28 00:36:18 -0700

    added code to modal with form

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -6,23 +6,3 @@ import { useContext } from "react";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 import "./WeatherCard.css";
 import { weatherOptions } from "../../utils/constants";
-
-const WeatherCard = ({ day, type = "cloudy", temperature }) => {
-  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
-  const weatherOption = weatherOptions.find((item) => {
-    return item.type === type && item.day === day;
-  });
-
-  const imageSrc = weatherOption.url;
-  return (
-    <div className="weatherCard">
-      <h3 className="weathercard__info">{`${temperature} ${currentTemperatureUnit.toUpperCase()}`}</h3>
-      <img
-        className="weathercard__image"
-        src={imageSrc}
-        alt={`Ilustration of weather card`}
-      ></img>
-    </div>
-  );
-};
-export default WeatherCard;
